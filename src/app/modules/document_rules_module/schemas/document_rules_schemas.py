8c7833a97d4bef958add84cbf72d9283d0051f34
Dev <dev@Mac-mini-de-Dev.local>
2025-05-29 16:59:05 -0500

    primeras validaciones para las reglas de documentos

--- src/app/modules/document_rules_module/schemas/document_rules_schemas.py
+++ src/app/modules/document_rules_module/schemas/document_rules_schemas.py
@@ -1,7 +1,7 @@
 # Archivo generado automáticamente para document_rules - schemas
 from datetime import datetime
 from typing import List, Optional, Dict, Any, Literal, Union
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, root_validator
 from src.app.shared.bases.base_schema import BaseOutSchema
 
 class DocumentRuleBase(BaseModel):
@@ -16,6 +16,47 @@ class DocumentRuleBase(BaseModel):
     max_file_size: Optional[int] = Field(None, ge=0, description="Tamaño máximo del archivo en MB")
     state: int = Field(1, ge=0, description="Estado activo (1) o inactivo (0)")
     
+    
+    @root_validator(pre=True)
+    def check_fields(cls, values):
+        try:
+            if not isinstance(values, dict): return values
+
+            if "name" not in values or not values["name"] or not str(values["name"]).strip():
+                raise Exception("El nombre de la regla es requerida y no puede estar vacía.")
+            elif not isinstance(values["name"], str):
+                raise Exception("El nombre de la regla debe ser una cadena de texto.")
+            
+            elif not isinstance(values["project_id"], int) or values["project_id"] < 0:
+                raise Exception("El poyecto debe ser un número entero positivo.")
+            elif "project_id" not in values or values["project_id"] is None:
+                raise Exception("El poyecto es requerido.")
+            elif not isinstance(values["project_id"], int) or values["project_id"] < 0:
+                raise Exception("El poyecto debe ser un número entero positivo.")
+
+            elif "document_type_id" not in values or values["document_type_id"] is None:
+                raise Exception("El tipo de documento es requerido.")
+            elif not isinstance(values["document_type_id"], int) or values["document_type_id"] < 0:
+                raise Exception("El tipo de documento debe ser un número entero positivo.")
+
+            elif "entity_type_id" not in values or values["entity_type_id"] is None:
+                raise Exception("El tipo de entidad es requerido.")
+            elif not isinstance(values["entity_type_id"], int) or values["entity_type_id"] < 0:
+                raise Exception("El tipo de entidad debe ser un número entero positivo.")
+
+            elif "stage_id" not in values or values["stage_id"] is None:
+                raise Exception("El tipo de etapa es requerido.")
+            elif not isinstance(values["stage_id"], int) or values["stage_id"] < 0:
+                raise Exception("El tipo de etapa debe ser un número entero positivo.")
+
+            if "max_file_size" in values and values["max_file_size"] is not None:
+                if not isinstance(values["max_file_size"], int) or values["max_file_size"] < 0:
+                    raise Exception("El tamaño del archivo debe ser un número entero positivo.")
+        
+            return values
+        except Exception as e:
+            raise e
+       
     
 class DocumentRuleCreate(DocumentRuleBase):
     pass
@@ -32,6 +73,47 @@ class DocumentRuleUpdate(BaseModel):
     is_required: bool = Field(False, description="¿Es requerido?")
     max_file_size: Optional[int] = Field(None, ge=0, description="Tamaño máximo del archivo en MB")
     state: int = Field(1, ge=0, description="Estado activo (1) o inactivo (0)")
+
+
+    @root_validator(pre=True)
+    def check_fields(cls, values):
+        try:
+            if not isinstance(values, dict): return values
+
+            if "name" not in values or not values["name"] or not str(values["name"]).strip():
+                raise Exception("El nombre de la regla es requerida y no puede estar vacía.")
+            elif not isinstance(values["name"], str):
+                raise Exception("El nombre de la regla debe ser una cadena de texto.")
+            
+            elif not isinstance(values["project_id"], int) or values["project_id"] < 0:
+                raise Exception("El poyecto debe ser un número entero positivo.")
+            elif "project_id" not in values or values["project_id"] is None:
+                raise Exception("El poyecto es requerido.")
+            elif not isinstance(values["project_id"], int) or values["project_id"] < 0:
+                raise Exception("El poyecto debe ser un número entero positivo.")
+
+            elif "document_type_id" not in values or values["document_type_id"] is None:
+                raise Exception("El tipo de documento es requerido.")
+            elif not isinstance(values["document_type_id"], int) or values["document_type_id"] < 0:
+                raise Exception("El tipo de documento debe ser un número entero positivo.")
+
+            elif "entity_type_id" not in values or values["entity_type_id"] is None:
+                raise Exception("El tipo de entidad es requerido.")
+            elif not isinstance(values["entity_type_id"], int) or values["entity_type_id"] < 0:
+                raise Exception("El tipo de entidad debe ser un número entero positivo.")
+
+            elif "stage_id" not in values or values["stage_id"] is None:
+                raise Exception("El tipo de etapa es requerido.")
+            elif not isinstance(values["stage_id"], int) or values["stage_id"] < 0:
+                raise Exception("El tipo de etapa debe ser un número entero positivo.")
+
+            if "max_file_size" in values and values["max_file_size"] is not None:
+                if not isinstance(values["max_file_size"], int) or values["max_file_size"] < 0:
+                    raise Exception("El tamaño del archivo debe ser un número entero positivo.")
+        
+            return values
+        except Exception as e:
+            raise e
     
 class DocumentRuleOut(DocumentRuleBase, BaseOutSchema):
     id: int = Field(ge=1)
--- src/app/modules/document_rules_module/services/document_rules_service.py
+++ src/app/modules/document_rules_module/services/document_rules_service.py
@@ -1,8 +1,12 @@
 # Archivo generado automáticamente para document_rules - services
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from src.app.modules.attributes_module.services.attributes_service import AttributeService
 from src.app.modules.document_rules_module.models.document_rules import DocumentRule
 from src.app.modules.document_rules_module.repositories.document_rules_repository import DocumentRuleRepository
 from src.app.modules.document_rules_module.schemas.document_rules_schemas import DocumentRuleOut
+from src.app.modules.entity_types_module.services.entity_type_service import EntityTypeService
+from src.app.modules.projects_module.services.projects_service import ProjectService
 from src.app.shared.bases.base_service import BaseService
 from typing import List, Optional, Dict, Any,Tuple
 from sqlalchemy.future import select
@@ -17,7 +21,9 @@ class DocumentRuleService(BaseService[DocumentRule, DocumentRuleOut]):
             out_schema=DocumentRuleOut,
         )
         self.db_session=db_session
-
+        self.project_service = ProjectService(db_session)
+        self.entity_type_service = EntityTypeService(db_session)
+        self.attribute_service = AttributeService(db_session)
     
     async def get_all(
         self,
@@ -51,5 +57,37 @@ class DocumentRuleService(BaseService[DocumentRule, DocumentRuleOut]):
             # si no, simplemente haces len().
             total = len(items)
             return items, total
+        except Exception as e:
+            raise e
+        
+    async def _validate_data(self, data):
+        try:
+            project_id = data.get("project_id")
+            entity_type_id = data.get("entity_type_id")
+            document_type_id = data.get("document_type_id")
+            project = await self.project_service.get_by_id(project_id)
+            if not project:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"No se encontró el proyecto con el id '{project_id}'",
+            )
+            
+            entity_type = await self.entity_type_service.get_by_id(entity_type_id)
+            if not entity_type:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"No se encontró el tipo de entidad con el id '{entity_type_id}'",
+            )
+
+            return True
+        except Exception as e:
+            raise e
+        
+
+    async def create(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        try:
+            await self._validate_data(data)
+            item = await self.repo.create(data)
+            return self.out_schema.model_validate(item).model_dump()
         except Exception as e:
             raise e
