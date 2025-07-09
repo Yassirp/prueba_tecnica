251de39527a5b2e5e19ec10cd4cb7073db565997
OMONSALVO <omonsalvo@naoweesuite.com>
2025-07-09 08:41:43 -0500

    Refactor db_seed.py to use parameter_values_seeder for attribute seeding; remove type_parent_seeder and add parameter_values_routes.py for new parameter management.

--- src/app/command/db_seed.py
+++ src/app/command/db_seed.py
@@ -9,9 +9,8 @@ from src.app.modules.ubication_module.seeders.municipality_seeder import Municip
 from src.app.modules.permission_module.seeders.security_seeder import SecuritySeeder
 from src.app.modules.user_module.seeders.user_seeder import UserSeeder
 from src.app.modules.parameters_module.seeders.attribute_seeder import AttributeSeeder
-from src.app.modules.parameters_module.seeders.documents_media_type_seeder import parameters as document_type_parameters
+from src.app.modules.parameters_module.seeders.parameter_values_seeder import parameters as parameter_values_parameters
 from src.app.modules.flow_module.seeders.state_user_relationship_seed import StateUserRelationshipSeeder
-from src.app.modules.parameters_module.seeders.type_parent_seeder import parameters as type_parent_parameters
 
 class Command(BaseCommandAsync):
     help = "Corre los seeders necesarios para el funcionamiento de la aplicación"
@@ -30,10 +29,9 @@ class Command(BaseCommandAsync):
             seeders = {
                 "CountrySeeder": CountrySeeder(),
                 "DepartmentSeeder": DepartmentSeeder(),
+                "AttributeSeeder": AttributeSeeder(parameter_values_parameters),
                 "MunicipalitySeeder": MunicipalitySeeder(),
                 "SecuritySeeder": SecuritySeeder(),
-                "AttributeSeeder": AttributeSeeder(document_type_parameters),
-                "AttributeSeeder": AttributeSeeder(type_parent_parameters),
                 "StateUserRelationshipSeeder": StateUserRelationshipSeeder(),
                 "UserSeeder": UserSeeder(),
             }
--- src/app/modules/parameters_module/seeders/parameter_values_seeder.py
+++ src/app/modules/parameters_module/seeders/parameter_values_seeder.py
@@ -19,6 +19,24 @@ parameters = {
             {"reference": "office_photo", "value": "Foto de sede"},
             {"reference": "other", "value": "Otro"},
         ]
-    }
+    },    
+    "type_parent": {
+        "name": "Tipo de relación entre usuarios",
+        "values": [
+            {"reference": "parent", "value": "Padre"},
+            {"reference": "mother", "value": "Madre"},
+            {"reference": "guardian", "value": "Tutora"},
+            {"reference": "boy_friend", "value": "Novio"},
+            {"reference": "girl_friend", "value": "Novia"},
+            {"reference": "brother", "value": "Hermano"},
+            {"reference": "sister", "value": "Hermana"},
+            {"reference": "grandparent", "value": "Abuelo"},
+            {"reference": "grandmother", "value": "Abuela"},
+            {"reference": "grandchild", "value": "Nieto"},
+            {"reference": "granddaughter", "value": "Nieto"},
+            {"reference": "grandson", "value": "Nieto"},
+            {"reference": "other", "value": "Otro"},
+        ]
+    },
 }
 
