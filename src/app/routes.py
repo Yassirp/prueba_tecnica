ea8f9e515df502eeccdc41b822d6fc6378934ee2
OMONSALVO <omonsalvo@naoweesuite.com>
2025-07-18 16:11:13 -0500

    Update routes.py to include living_group_user_routes: Add import and router inclusion for managing user interactions within living groups, enhancing the routing structure for better organization.

--- src/app/routes.py
+++ src/app/routes.py
@@ -8,6 +8,7 @@ from src.app.modules.permission_module.routes.role_routes import router as role_
 from src.app.modules.user_module.routes.report_routes import router as report_routes 
 from src.app.modules.sedes_module.routes.sedes_routes import router as sedes_routes
 from src.app.modules.living_group_module.routes.living_group_routes import router as living_group_routes
+from src.app.modules.living_group_module.routes.living_group_user_routes import router as living_group_user_routes
 
 router = APIRouter()
 
@@ -20,3 +21,4 @@ router.include_router(role_routes)
 router.include_router(report_routes)
 router.include_router(sedes_routes)
 router.include_router(living_group_routes)
+router.include_router(living_group_user_routes)
